97761a111d2c0f7018405d24abd29fda13807c83
ButtersSharky <ciarraparker54@gmail.com>
2024-10-02 13:34:16 -0700

    added code to the main menu screen

--- popcorn/Assets/Scripts/GameManager.cs
+++ popcorn/Assets/Scripts/GameManager.cs
@@ -1,18 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+       
 public class GameManager : MonoBehaviour
 {
+    public bool isPaused = false;
+
+    public GameObject pauseMenu;
+    public PlayerController playerdata;
+
+    public Image healthBar;
+    public TextMeshProUGUI clipCounter;
+    public TextMeshProUGUI ammoCounter;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (SceneManager.GetActiveScene().buildIndex > 0)
+            playerdata = GameObject.Find("popcorn").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (SceneManager.GetActiveScene().buildIndex > 0)
+        {
+            healthBar.fillAmount = Mathf.Clamp((float)playerdata.health / (float)playerdata.maxHealth, 0, 1);
+            if (playerdata.weaponID < 0)
+            {
+                clipCounter.gameObject.SetActive(false);
+                ammoCounter.gameObject.SetActive(false);
+            }
+
+            else
+            {
+                clipCounter.gameObject.SetActive(true);
+                ammoCounter.gameObject.SetActive(true);
+
+                clipCounter.text = "clip: " + playerdata.currentClip + "/" + playerdata.clipsize;
+                ammoCounter.text = "Ammo: " + playerdata.currentAmmo;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (!isPaused)
+                {
+                    pauseMenu.SetActive(true);
+
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+
+                    Time.timeScale = 0;
+
+                    isPaused = true;
+                }
+
+                else
+                    Resume();
+            }
+        }                          
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+
+        Time.timeScale = 1;
+
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
+
+        isPaused = false;
+    }
+
+    public void LoadLevel(int sceneID)
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(sceneID);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void RestartLevel()
+    {
+        Time.timeScale = 1;
+        LoadLevel(SceneManager.GetActiveScene().buildIndex);
     }
 }
